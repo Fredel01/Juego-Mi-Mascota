ef6aa09b9f21c80540b15c26559515032da64015
Fredy Moran <elianmoran2019@gmail.com>
2025-03-28 23:25:20 -0600

    Agregamos mas comentarios Mascota.h

--- Mascota.h
+++ Mascota.h
@@ -1,10 +1,12 @@
 #ifndef MASCOTA_H
 #define MASCOTA_H
 #include <iostream>
-#include <windows.h>
 
 using namespace std;
-
+/**
+ * @class Mascota
+ * @brief Implementa el simulador de Mascota
+ */
 class Mascota {
 private:
     string nombre;
@@ -52,36 +54,50 @@ int get_felicidad(int felicidad) { return felicidad; }
 void Mascota::set_nombre(string _nombre) { 
     Mascota::nombre = _nombre; 
 }
+/**
+ * @brief verificamos para modificar los valores
+ * @param nuevaEnergia ingresada por el usuario
+ */
 void Mascota::set_energia(int nuevaEnergia) { 
-    if (nuevaEnergia < 0) Mascota::energia = 0; 
-    else if (nuevaEnergia > 100) Mascota::energia = 100;
-    else Mascota::energia = nuevaEnergia;
+    if (nuevaEnergia < 0) Mascota::energia = 0; //Evita valores negativos
+    else if (nuevaEnergia > 100) Mascota::energia = 100; // Limite maximo
+    else Mascota::energia = nuevaEnergia; //se asigna el nuevo valor
 }
+/**
+ * @brief verificamos para modificar los valores
+ * @param nuevoHambre ingresada por el usuario
+ */
 void Mascota::set_hambre(int nuevoHambre) { 
-    if (nuevoHambre < 0) Mascota::hambre = 0;   // Evita valores negativos
-    else if (nuevoHambre > 100) Mascota::hambre = 100; // Límite máximo
+    if (nuevoHambre < 0) Mascota::hambre = 0;   
+    else if (nuevoHambre > 100) Mascota::hambre = 100; 
     else Mascota::hambre = nuevoHambre;
 }    
+//para modificar la felicidad
 void Mascota::set_felicidad(int nuevaFelicidad) { 
     if (nuevaFelicidad < 0) Mascota::felicidad = 0;
     else if (nuevaFelicidad > 100) Mascota::felicidad = 100;
     else Mascota::felicidad = nuevaFelicidad;
 }
-// Método para mostrar el estado de la mascota
+/**
+ * @brief Método para mostrar el estado de la mascota
+ */
 void Mascota::estado_de_la_mascota() {
-    SetConsoleOutputCP(CP_UTF8);
     cout << "Nombre: " << nombre << endl
          << "Energia: " << energia << endl 
          << "Hambre: " << hambre << endl
          << "Felicidad: " << felicidad << endl;
-    if (hambre > 80) cout << "\nTengo hambre (ᗒᗣᗕ)՞" << endl;
-    if (energia < 10) cout << "\nEstoy muy cansado (｡•́︿•̀｡)" << endl;
-    if (felicidad < 10) cout << "\nJuguemos (◞ ‸ ◟)" << endl;
+    if (hambre > 80) cout << "\nTengo hambre (T_T)" << endl;
+    if (energia < 10) cout << "\nEstoy muy cansado (-_-)" << endl;
+    if (felicidad < 10) cout << "\nJuguemos (^o^)" << endl;
 }
+/**
+ * @brief Permite seleccionar que alimento dar a la mascota
+ * @return Para regresar al menu principal
+ */
 void Mascota::comer() {
-    int opcion;
+    int opcion; //Variable para manejar la opcion del usuario
     
-    if (hambre == 0) {
+    if (hambre == 0) { //Verificar que no sea 0 antes de comer
         cout << "\nTu mascota no tiene hambre en este momento. Vuelve mas tarde \n Presiona Enter para continuar... \n";
         cin.ignore();
         cin.get();
@@ -101,7 +117,7 @@ void Mascota::comer() {
     if (cin.fail()) {
         cin.clear();  // Restablece el estado de cin
         cin.ignore(10000, '\n');  // Limpia el buffer de entrada
-        cout << "Entrada inválida. Inténtalo de nuevo.\n";
+        cout << "Entrada invalida. Intentalo de nuevo.\n";
         return;
     }
 
@@ -111,24 +127,27 @@ void Mascota::comer() {
         case 3: set_hambre(hambre - 5); set_energia(energia + 5); break;
         case 4: set_hambre(hambre - 50); set_energia(energia + 50); break;
         default:
-            cout << "Opción no válida. Inténtalo de nuevo (Presiona 'Enter' para continuar...\n";
+            cout << "Opcion no valida. Intentalo de nuevo (Presiona 'Enter' para continuar...\n";
             cin.ignore(); cin.get();
             break;
     }
     cout << "\nGracias por alimentarme (^o^)\n"; cout << "Presiona 'Enter' para continuar...\n";
     cin.ignore(); cin.get();
 }
+/**
+ * @brief Permite jugar con la mascota
+ */
 void Mascota::jugar() {
     if (energia <= 15) { cout << nombre << " esta muy cansado. Vuelve mas tarde... \n" << endl; cin.ignore();
         cin.get(); return; }
     if (hambre >= 80) { cout << nombre << " tiene mucha hambre :( \n" << endl; cin.ignore(); cin.get(); return; }
 
-    int opcion;
-    cout << "\n¿Que vamos a jugar hoy ✧*｡٩(ˊᗜˋ*)و✧*｡?" << endl;
+    int opcion; //tambien manejamos la opcion del usuario con switch
+    cout << "\nQue vamos a jugar hoy? (^o^)" << endl;
     cout << "\n1. Atrapar la pelota (+30 felicidad, +10 hambre, -25 energia)" << endl;
     cout << "2. Perseguir gallinas (+40 felicidad, +20 hambre, -50 energia)\n";
     cout << "3. Escondidas (+20 felicidad, +10 hambre, -15 energia)\n";
-    cin >> opcion;
+    cin >> opcion; 
 
     if (cin.fail()) {
         cin.clear();
@@ -147,13 +166,19 @@ void Mascota::jugar() {
         break;
     }
 } 
+/**
+ * @brief Permite recuperar energias durmiendo
+ */
 void Mascota::dormir() {
     if (energia == 100) { cout << nombre << " no tiene sueño... "; cin.ignore(); cin.get(); return; }
     cout << nombre << " esta durmiendo..." << endl;
-    energia = 100;
+    energia = 100; //si tiene las energias al 100 no tiene por qué dormir
     cout << "Presiona Enter para continuar";
     cin.ignore();
     cin.get();
 }
+/**
+ * @brief Evita acumulaciones. Limpia pantalla
+ */
 void Mascota::limpiar_pantalla() { cout << "\033[2J\033[H"; }
 #endif
